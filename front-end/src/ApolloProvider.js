092ea3cdb7089d5d372d5a0ab382e2a574422a26
cnb1 <hackerrank134@gmail.com>
2022-05-14 13:04:28 -0400

    created front end

---
+++ front-end/src/ApolloProvider.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import App from './App';
+import {ApolloClient} from '@apollo/client';
+import {InMemoryCache} from '@apollo/client';
+import {createHttpLink} from '@apollo/client';
+import {ApolloProvider} from '@apollo/client';
+import {setContext} from 'apollo-link-context';
+
+
+const httplink = createHttpLink({
+    uri: 'http://localhost:3005'
+})
+
+const authLink = setContext(() => {
+    const token = localStorage.getItem('jwtToken');
+    return{
+        headers: {
+            Authorization: token ? `Bearer ${token}` : ''
+        }
+    }
+})
+
+const client = new ApolloClient({
+    link: authLink.concat(httplink),
+    cache: new InMemoryCache()
+})
+
+
+
+export default (
+    <ApolloProvider client={client}>
+        <App/>
+    </ApolloProvider>
+)
--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,14 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import ReactDOM from 'react-dom';
 import reportWebVitals from './reportWebVitals';
+import ApolloProvider from './ApolloProvider';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+ReactDOM.render(
+  ApolloProvider,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
