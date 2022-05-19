31ace7ec54364695f895c6ef897231c8a595839b
cnb1 <hackerrank134@gmail.com>
2022-05-19 11:59:01 -0400

    small changes

--- back-end/graphql/resolvers/userportfolio.js
+++ back-end/graphql/resolvers/userportfolio.js
@@ -12,9 +12,7 @@ module.exports = {
             if (userportfolio) {
                 throw new UserInputError('Portfolio exists');
             }
-            else {
-                console.log('portfolio id doesnt exitst')
-            }
+            
 
             const newPortfolio = new UserPortfolio({
                 username,
@@ -58,7 +56,6 @@ module.exports = {
     Query: {
         async getPortfolios() {
             try {
-                console.log("in get portfolios");
                 const portfolios = await UserPortfolio.find();
                 return portfolios;
             }
