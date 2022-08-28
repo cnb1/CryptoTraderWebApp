32a6f9352c9d571d6d6d70d6332c8256f84b9c04
cnb1 <hackerrank134@gmail.com>
2022-08-28 20:31:56 +0200

    update on post for start

--- front-end/src/components/StartTrading.js
+++ front-end/src/components/StartTrading.js
@@ -11,28 +11,44 @@ function StartTrading() {
 
   function handleClick(amount) {
     console.log("handling money click");
-    console.log(state)
+    console.log(state);
     /*  
         make request to start the program
     */
+    if (state >= 1000000) {
+      console.log("call start");
+      const requestOptions = {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          userid: "1234",
+          strategy: "ma15",
+          money: 1000000,
+        }),
+      };
+      fetch("http://127.0.0.1:8080/start", requestOptions)
+        .then((response) => response.json())
+        .then((data) => this.setState({ postId: data.id }));
+    } else {
+        alert("amount needs to be over 1 million")
+      console.log("dont call start");
+    }
   }
 
   function callback(value) {
-    let isnum = /^\d+$/.test(value)
+    let isnum = /^\d+$/.test(value);
 
     if (isnum) {
-        console.log('it is a number')
-        if (value > 0) {
-            setState(value)
-        }
-        else {
-            setState(-1)
-        }
+      console.log("it is a number");
+      if (value > 0) {
+        setState(value);
+      } else {
+        setState(-1);
+      }
+    } else {
+      console.log("it is not a number");
+      setState(-1);
     }
-    else {
-        console.log('it is not a number')
-        setState(-1)
-    }    
   }
 
   return (
