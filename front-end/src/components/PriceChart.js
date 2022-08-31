0a116273ed2179afb1be0175cbb1944ba4259973
cnb1 <hackerrank134@gmail.com>
2022-08-31 03:37:27 -0400

    custom dark mode

--- front-end/src/components/PriceChart.js
+++ front-end/src/components/PriceChart.js
@@ -3,6 +3,7 @@ import gql from "graphql-tag";
 import { useQuery, useSubscription } from "@apollo/client";
 import PriceSub from './PriceSub';
 import Chart from "chart.js/auto";
+import '../styles/PriceChart.css'
 
 import { Line } from "react-chartjs-2";
 
@@ -20,17 +21,95 @@ function PriceChart({ items: { portfolioId } }) {
         },
     });
 
+    const CHART_COLORS = {
+        red: 'rgb(69, 2, 2)',
+        orange: 'rgb(255, 159, 64)',
+        yellow: 'rgb(255, 205, 86)',
+        green: 'rgb(2, 54, 28)',
+        blue: 'rgb(54, 162, 235)',
+        purple: 'rgb(153, 102, 255)',
+        grey: 'rgb(56, 56, 56)'
+    };
+
     const options = {
         responsive: true,
+        animations: {
+            tension: {
+                duration: 1000,
+                easing: 'linear',
+                from: 1,
+                to: 0,
+                loop: true
+            }
+        },
         plugins: {
             legend: {
                 position: "bottom",
+                labels: {
+                    // This more specific font property overrides the global property
+                    font: {
+                        size: 20
+                    }
+                }
             },
             title: {
-                display: true,
+                fontSize:40,
+                display: false,
                 text: "Balance",
+                labels: {
+                    // This more specific font property overrides the global property
+                    font: {
+                        size: 30
+                    }
+                }
             },
         },
+        scales: {
+            yAxes: [{
+                ticks: {
+                    fontSize: 40
+                }
+            }],
+            x: {
+                ticks: {
+                    font: {
+                      size: 20
+                    }
+                  },
+                  title: {
+                    display: true,
+                    text: 'Date',
+                    font: {
+                      size: 25
+                    }
+                  }
+            },
+            y: {
+                ticks: {
+                    font: {
+                      size: 20
+                    }
+                  },
+                  title: {
+                    display: true,
+                    text: 'Amount in USD',
+                    font: {
+                      size: 25
+                    }
+                  },
+                grid: {
+                    drawBorder: false,
+                    color: function (context) {
+                        if (context.tick.value > 0) {
+                            return CHART_COLORS.red;
+                        } else if (context.tick.value < 0) {
+                            return CHART_COLORS.red;
+                        }
+                        return '#000000';
+                    },
+                }
+            }
+        }
     };
 
     if (portfolio) {
@@ -52,6 +131,7 @@ function PriceChart({ items: { portfolioId } }) {
 
         return (
             <>
+                <h1 className="title-name">Balance</h1>
                 <Line data={data} options={options} />
             </>
         );
